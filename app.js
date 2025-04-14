a49f9d2303ba85f7bb8378252b4d3979bc53b8f0
Puupalikka <stepanovjegor2@gmail.com>
2025-04-14 17:05:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
- function main() {
-	let nappi = document.getElementById('rekisterointi');
-	nappi.addEventListener('click', lisaa);
+function main() {
+	if (!(localStorage.getItem('nimi'))) {
+		let nappi = document.getElementById('rekisterointi');
+		nappi.addEventListener('click', function() {toinen_sivu(document.getElementById('nimi').value)});
+	}else{
+		toinen_sivu(localStorage.getItem('nimi'));
+	}
 }
 
-function lisaa() {
-	let nimi = document.getElementById('nimi').value;
-	let listanElementti = document.createElement('li')
-	listanElementti.textContent = nimi;
-	listanElementti.className = 'listan_osa';
-	document.getElementById('lista').append(listanElementti);
+function toinen_sivu(nimi) {
+	while (document.body.firstChild) {
+		document.body.removeChild(document.body.firstChild);
+	}
+	
+	const hedari = document.createElement('header');
+	const tervehdys = document.createElement('p');
+	tervehdys.id = 'tekstikappale';
+	const poistonappi = document.createElement('button');
+	
+	poistonappi.textContent = 'Poista data';
+	poistonappi.id = 'nappi';
+	poistonappi.addEventListener('click', poista);
+	
+	tervehdys.textContent = 'Tervetuloa ' + nimi + '!';
+	
+	document.body.appendChild(hedari);
+	document.body.appendChild(tervehdys);
+	document.body.appendChild(poistonappi);
+	
+	localStorage.setItem('nimi', nimi);
+}
+
+function poista() {
+	localStorage.removeItem('nimi');
+	document.getElementById('tekstikappale').textContent = 'Data poistettu!';
 }
 
 main();
